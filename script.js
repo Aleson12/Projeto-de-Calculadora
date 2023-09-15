cf8a2fd67cdf12096add00d4e00b621f9ed57965
Aleson Felizardo <81363714+Aleson12@users.noreply.github.com>
2023-09-15 17:00:31 -0300

    Atualização do projeto
    As funcionalidades foram adicionadas.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 let display = document.getElementById("result"); /* obtendo a secção onde aparecerão os números  */
-let button = document.querySelectorAll(".btn"); /* obtendo todos os botões pela "class" e
-inserindo em uma variável mutável chamada "button" */
+let valor = "";
 
-for (let btn of button){ 
-  btn.addEventListener("click", () => { 
-  display.innerText += btn.value;
-  }) 
-}    
+function calculando(v) {
+  valor = valor + v;
+  display.innerText = valor;
+} 
 
 function cancel() {
-  display.innerHTML = display.innerHTML.substring(0, display.innerHTML.length - 1);
+  display.innerText = display.innerText.substring(0,display.innerText.length - 1);
   /* obtendo o valor interno do display (onde os números aparecem), o qual recebe
-  ele mesmo, porém, contando todo o seu tamanho e subtraindo da direita à esquerda.  */
-}    
-/*
-function sum(){
+  ele mesmo, porém, contando todo o seu tamanho e subtraindo da direita à esquerda.  
+*/
+  valor = "";  /* esta instrução faz com que, após deletar um valor (número),
+  o valor anterior não concatene com o novo! */
+}
 
-}*/
+function equal() {
+  try {
+    let resultado = eval(valor);
+    display.innerText = resultado;
+    valor = resultado.toString(); 
+  } catch (error) {
+    display.innerText = "error";
+  }
+}   
